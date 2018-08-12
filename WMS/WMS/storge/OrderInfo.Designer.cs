b34767ffedf9782e377434be522a406eea293aae
0455YYB <tugueiyin@163.com>
2018-08-13 00:44:07 +0800

    2018-08-12

--- WMS/WMS/storge/OrderInfo.Designer.cs
+++ WMS/WMS/storge/OrderInfo.Designer.cs
@@ -32,9 +32,10 @@
             // 
             // OrderInfo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1310, 666);
+            this.ClientSize = new System.Drawing.Size(941, 550);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "OrderInfo";
             this.Text = "OrderInfo";
             this.ResumeLayout(false);
